ff935089a16c337f4d8c1c10b90016f647095519
Aydik <Nekkisuper@gmail.com>
2025-07-23 16:42:28 +0400

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,45 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import prettier from 'eslint-plugin-prettier';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      js,
+      react: pluginReact,
+      'react-hooks': pluginReactHooks,
+      'jsx-a11y': jsxA11y,
+      prettier,
+    },
+    rules: {
+      ...pluginReact.configs.flat.recommended.rules,
+      ...jsxA11y.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'prettier/prettier': 'warn',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  tseslint.configs.recommended,
+]);
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/t1-homework-4/',
+  plugins: [react()],
+  css: {
+    postcss: './postcss.config.js',
+  },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, './src'),
+      app: path.resolve(__dirname, './src/app'),
+      pages: path.resolve(__dirname, './src/pages'),
+      widgets: path.resolve(__dirname, './src/widgets'),
+      features: path.resolve(__dirname, './src/features'),
+      entities: path.resolve(__dirname, './src/entities'),
+      shared: path.resolve(__dirname, './src/shared'),
+    },
+  },
+});
